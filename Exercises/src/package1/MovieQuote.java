2f5843267314aaf767a17083f7f8fa734aab6240
JackReis01 <jackreis2019@gmail.com>
2018-08-27 09:46:57 -0500

    updated text

---
+++ Exercises/src/package1/MovieQuote.java
@@ -0,0 +1,14 @@
+package package1;
+import javax.swing.JOptionPane;
+
+public class MovieQuote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,  "This is not a meme this is a cry for help");
+	
+		
+
+	}
+
+}
---
+++ Exercises/src/package1/tablesandchairs.java
@@ -0,0 +1,35 @@
+package package1;
+
+public class tablesandchairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		//something
+		
+		System.out.println("X                   X");
+		System.out.println("X                   X");
+		System.out.println("X     XXXXXXXXX     X");
+		System.out.println("XXXXX X       X XXXXX");
+		System.out.println("X   X X       X X   X");
+		System.out.println("X   X X       X X   X");
+	
+		displayTruck();
+	
+	}
+	
+	
+	public static void displayTruck() {
+		System.out.println("  /XXXXXXXXXXXXXXXXXX");
+		System.out.println("/XXXXXXXXXXXXXXXXXXXX");
+		System.out.println("XX   XXXXXX \TACO/ XX");
+		System.out.println("XX   XXXXXX--------XX");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("XXX/OO\XXXXXXXXX/OO\X");
+		System.out.println("XXX OO XXXXXXXXX OO X");
+	
+
+	}
+		
+		
+}
