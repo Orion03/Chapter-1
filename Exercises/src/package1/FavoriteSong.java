75d45726b72551a4a59d5b094c1676f20fbe7194
JackReis01 <jackreis2019@gmail.com>
2018-08-28 08:59:05 -0500

    Updated text

--- Exercises/src/package1/FavoriteSong.java
+++ Exercises/src/package1/FavoriteSong.java
@@ -1,5 +1,15 @@
 package package1;
+import javax.swing.JOptionPane;
 
 public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,  "Are you OK Annie?");
+		JOptionPane.showMessageDialog(null,  "You've been hit by");
+		JOptionPane.showMessageDialog(null,  "You've struck by");
+		JOptionPane.showMessageDialog(null,  "A smooth criminal");
+
+	}
 
 }
