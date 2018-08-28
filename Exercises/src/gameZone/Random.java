139f0483a1c35e810a87509bd71cb05903fa4682
JackReis01 <jackreis2019@gmail.com>
2018-08-28 09:44:08 -0500

    updated text

---
+++ Exercises/src/gameZone/Random.java
@@ -0,0 +1,13 @@
+package gameZone;
+import javax.swing.JOptionPane;
+
+public class Random {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "The number is "+ (1 + (int)(Math.random() * 10)));;
+		
+
+	}
+
+}
