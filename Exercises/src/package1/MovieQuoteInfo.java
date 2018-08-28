a233095f660a783ba6bf6cfff227b854c5fe6cb6
JackReis01 <jackreis2019@gmail.com>
2018-08-28 08:47:52 -0500

    updated text

--- Exercises/src/package1/MovieQuoteInfo.java
+++ Exercises/src/package1/MovieQuoteInfo.java
@@ -1,8 +1,11 @@
+package package1;
+import javax.swing.JOptionPane;
 
 public class MovieQuoteInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,  "What's in the box?");
 
 	}
 
