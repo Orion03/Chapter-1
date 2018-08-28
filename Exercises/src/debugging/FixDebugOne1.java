7661740670333f2a357fed4b4d9fd687f1d37559
JackReis01 <jackreis2019@gmail.com>
2018-08-28 09:10:40 -0500

    Updated text

---
+++ Exercises/src/debugging/FixDebugOne1.java
@@ -0,0 +1,11 @@
+package debugging;
+
+public class FixDebugOne1 {
+
+	public static void main(String[] args) {
+		/*This Program displays a greeting */
+		System.out.println("Hello");
+
+	}
+
+}
--- Exercises/src/package1/MovieQuote.java
+++ Exercises/src/package1/MovieQuote.java
@@ -5,7 +5,7 @@ public class MovieQuote {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null,  "This is not a meme this is a cry for help");
+		JOptionPane.showMessageDialog(null,  "What's in the box?");
 	
 		
 
