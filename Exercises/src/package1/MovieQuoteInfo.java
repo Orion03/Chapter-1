adf9ebf3df1e3b9443c0caecbd8f69bfc87c008c
JackReis01 <jackreis2019@gmail.com>
2018-08-28 08:53:02 -0500

    Updated text

--- Exercises/src/package1/MovieQuoteInfo.java
+++ Exercises/src/package1/MovieQuoteInfo.java
@@ -5,7 +5,7 @@ public class MovieQuoteInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null,  "What's in the box?");
+		JOptionPane.showMessageDialog(null,  "Seven");
 
 	}
 
