23cc311afaa2f27982dc3a2d19567d52f36f2371
JackReis01 <jackreis2019@gmail.com>
2018-08-28 09:29:20 -0500

    updated text

---
+++ Exercises/src/caseProblems/SammysMotto.java
@@ -0,0 +1,13 @@
+package caseProblems;
+
+public class SammysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ss Ss Ss Ss Ss Ss Ss Ss Ss Ss Ss Ss Ss");
+		System.out.println("Ss Sammy's makes it fun in the sun. Ss");
+		System.out.println("Ss Ss Ss Ss Ss Ss Ss Ss Ss Ss Ss Ss Ss");
+
+	}
+
+}
---
+++ Exercises/src/caseProblems/SammysMotto2.java
@@ -0,0 +1,11 @@
+package caseProblems;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sammy's makes it fun in the sun.");
+
+	}
+
+}
