8ab1666a915bbe60e3b269edbc5f85e9a6464571
JackReis01 <jackreis2019@gmail.com>
2018-08-29 09:35:42 -0500

    updated text

--- Chapter 2/src/youDoIt/IntegerDemo.java
+++ Chapter 2/src/youDoIt/IntegerDemo.java
@@ -8,6 +8,7 @@ public class IntegerDemo {
 		byte aByte = 12;
 		short aShort = 12;
 		long aLong = 1234567890987654321L;
+		int anotherInt = anInt * 10000000;
 		
 		System.out.println("The int is " + anInt);
 		System.out.println("The byte is " + aByte);
