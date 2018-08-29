7de9ab76cd17a727c4f3b7c49dd83a0f28140ab2
JackReis01 <jackreis2019@gmail.com>
2018-08-29 09:17:51 -0500

    updated text

---
+++ Chapter 2/src/youDoIt/DataDemo.java
@@ -0,0 +1,13 @@
+package youDoIt;
+
+public class DataDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int aWholeNumber = 315;
+		System.out.print("the number is ");
+		System.out.println(aWholeNumber);
+
+	}
+
+}
