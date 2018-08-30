a7ad8fdc3887b0925e1984196097f9945a572a15
JackReis01 <jackreis2019@gmail.com>
2018-08-30 09:11:41 -0500

    updated text

--- Chapter 2/src/youDoIt/GetUserInfo.java
+++ Chapter 2/src/youDoIt/GetUserInfo.java
@@ -11,6 +11,8 @@ public class GetUserInfo {
 		Scanner inputDevice = new Scanner(System.in);
 		System.out.print("Please enter your name");
 		name = inputDevice.nextLine();
+		System.out.println("Please enter your age");
+		age = inputDevice.nextInt();
 
 	}
 
