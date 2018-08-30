16cdfef8bf03d696ff9066dc877655db0e82f1c3
JackReis01 <jackreis2019@gmail.com>
2018-08-30 09:08:40 -0500

    updated text

---
+++ Chapter 2/src/youDoIt/GetUserInfo.java
@@ -0,0 +1,15 @@
+package youDoIt;
+
+import java.util.Scanner;
+
+public class GetUserInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		int age;
+		Scanner inputDevice = new Scanner(System.in);
+
+	}
+
+}
