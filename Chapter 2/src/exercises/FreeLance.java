74386538b2e3699c3369456f0d21bb1a4792acde
JackReis01 <jackreis2019@gmail.com>
2018-08-30 09:46:49 -0500

    updated text

---
+++ Chapter 2/src/exercises/FreeLance.java
@@ -0,0 +1,24 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class FreeLance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1;
+		int num2;
+		int total;
+		
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your first number >> ");
+		num1 = input.nextLine();
+		System.out.println("Please enter your second number >> ");
+		num2 = input.nextLine();
+		
+
+	}
+
+}
