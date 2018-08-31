886222fd0a40d0c7980346870d46cb8dd9ee8dba
JackReis01 <jackreis2019@gmail.com>
2018-08-31 09:03:16 -0500

    updated text

---
+++ Chapter 2/src/youDoIt/SalaryDialog.java
@@ -0,0 +1,12 @@
+package youDoIt;
+
+public class SalaryDialog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String wageString;
+		String dependentString;
+
+	}
+
+}
