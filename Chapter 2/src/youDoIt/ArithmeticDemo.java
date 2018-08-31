890f7bf2f14d96e1f34e80c81ea61dca5c114f3f
JackReis01 <jackreis2019@gmail.com>
2018-08-31 09:24:49 -0500

    updated text

--- Chapter 2/src/youDoIt/ArithmeticDemo.java
+++ Chapter 2/src/youDoIt/ArithmeticDemo.java
@@ -1,9 +1,20 @@
 package youDoIt;
 
+import java.util.Scanner;
+
 public class ArithmeticDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int firstNumber;
+		int secondNumber;
+		int sum;
+		int difference;
+		int average;
+		
+		Scanner input = new Scanner(System.in);
+		
+		
 
 	}
 
