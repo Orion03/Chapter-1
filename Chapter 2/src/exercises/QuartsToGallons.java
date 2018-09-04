b22397fa2e4032ce18e6ee65d44934e587d9ad79
JackReis01 <jackreis2019@gmail.com>
2018-09-04 09:53:30 -0500

    updated text

--- Chapter 2/src/exercises/QuartsToGallons.java
+++ Chapter 2/src/exercises/QuartsToGallons.java
@@ -11,11 +11,12 @@ public class QuartsToGallons {
 		
 		final int Quarts;
 		final int Gallons = 4;
-		int QuartsToGallons = Quarts / Gallons;
-		int RemainingQuarts = Quarts % Gallons;
 		
 		System.out.println("How many quartz you have >> ");
 		Quarts = input.nextInt();
+		
+		int QuartsToGallons = Quarts / Gallons;
+		int RemainingQuarts = Quarts % Gallons;
 		
 		System.out.println("If something needs " + Quarts + " quarts it needs " + QuartsToGallons + 
 				" gallons with a remaining " + RemainingQuarts + " quarts.");
