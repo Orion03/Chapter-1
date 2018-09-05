c5a4da52e564546b0c09cbbddb5e359b3d88dcee
JackReis01 <jackreis2019@gmail.com>
2018-09-05 09:45:33 -0500

    updated text

---
+++ Chapter 2/src/exercises/Dollars.java
@@ -0,0 +1,15 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Dollars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+	}
+
+}
