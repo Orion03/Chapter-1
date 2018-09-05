9b1de9fa0d74ba7c3169c9085868c2418750e2f9
JackReis01 <jackreis2019@gmail.com>
2018-09-05 08:44:41 -0500

    updated text

---
+++ Chapter 2/src/exercises/NauticalMiles.java
@@ -0,0 +1,19 @@
+package exercises;
+
+public class NauticalMiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double NauticalMile;
+		
+		Scanner input = new Scanner(System.in);
+		
+		double Kilometer = 1.852 * NauticalMile;
+		double Mile = 1.150779 * NauticalMile;
+		
+		
+
+	}
+
+}
