d5985180e754c214db3aa1fd5b7ce495f7f40993
JackReis01 <jackreis2019@gmail.com>
2018-09-05 09:08:22 -0500

    updated text

---
+++ Chapter 2/src/exercises/Initials.java
@@ -0,0 +1,29 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		String Initial1;
+		String Initial2;
+		String Initial3;
+		
+		System.out.println("What is your first initial >> ");
+		Initial1 = input.next();
+		
+		System.out.println("What is your middle initial >> ");
+		Initial2 = input.next();
+		
+		System.out.println("What is your last initial >> ");
+		Initial3 = input.next();
+		
+		
+
+	}
+
+}
