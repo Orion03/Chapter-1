9c757d5f8697dbfe30586c015a94df7bed2a6e5b
JackReis01 <jackreis2019@gmail.com>
2018-09-05 09:01:12 -0500

    updated text

---
+++ Chapter 2/src/exercises/InchesToFeetInteractive.java
@@ -0,0 +1,28 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class InchesToFeetInteractive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		final int Inches;
+		final int Feet = 12;
+		
+		System.out.println("How many Inches are there >> ");
+		Inches = input.nextInt();
+		
+		int InchesToFeet = Inches / Feet;
+		int RemainingInches = Inches % Feet;
+		
+		System.out.println("If something needs " + Inches " inches it needs " + InchesToFeet + " feet with a remaining " + RemainingInches + 
+				" Inches.");
+		
+		
+
+	}
+
+}
