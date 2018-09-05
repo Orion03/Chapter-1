6bb423b010c58a51b46319a94f5a1eb9d23914ca
JackReis01 <jackreis2019@gmail.com>
2018-09-05 09:42:32 -0500

    updated text

--- Chapter 2/src/exercises/ChiliToGo.java
+++ Chapter 2/src/exercises/ChiliToGo.java
@@ -1,12 +1,15 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class ChiliToGo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner input = new Scanner(System.in);
 		
-
+		
 	}
 
 }
