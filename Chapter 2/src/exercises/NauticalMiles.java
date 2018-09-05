385045206ae9090c6a4c171d544a558d051f0176
JackReis01 <jackreis2019@gmail.com>
2018-09-05 08:50:36 -0500

    updated text

--- Chapter 2/src/exercises/NauticalMiles.java
+++ Chapter 2/src/exercises/NauticalMiles.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class NauticalMiles {
 
 	public static void main(String[] args) {
@@ -9,11 +11,14 @@ public class NauticalMiles {
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Insert a number of nautical miles >> ");
+		NauticalMile = input.nextInt();
+		
 		double Kilometer = 1.852 * NauticalMile;
 		double Mile = 1.150779 * NauticalMile;
 		
-		
-
+		System.out.println(NauticalMile + " nautical miles equals " + Kilometer + " kilometer(s) and " + 
+		Mile + " miles.");
 	}
 
 }
