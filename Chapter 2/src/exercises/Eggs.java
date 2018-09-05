16045fdaed7ecf9f57db68ba2d74297610a71afb
JackReis01 <jackreis2019@gmail.com>
2018-09-05 09:16:48 -0500

    updated text

--- Chapter 2/src/exercises/Eggs.java
+++ Chapter 2/src/exercises/Eggs.java
@@ -1,9 +1,18 @@
 package exercises;
 
+import java.util.Scanner
+
 public class Eggs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		double SingularEgg = .45;
+		double DozenOfEggs = 3.25;
+		
+		
 
 	}
 
