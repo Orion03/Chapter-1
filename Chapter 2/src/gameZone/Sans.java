094e511f21b40885b9389ad819197c44062f939d
JackReis01 <jackreis2019@gmail.com>
2018-09-07 09:22:11 -0500

    updated text

--- Chapter 2/src/gameZone/Sans.java
+++ Chapter 2/src/gameZone/Sans.java
@@ -5,13 +5,28 @@ public class Sans {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("           XXXXXXXXXXXX          ");
-		System.out.println("        XXX            XXX       ");
-		System.out.println("      XX                  XX     ");
-		System.out.println("     X                      X    ");
-		System.out.println("    X                        X   ");
-		System.out.println("   X                          X  ");
+		System.out.println("           XXXXXXXXXXXX           ");
+		System.out.println("        XXX            XXX        ");
+		System.out.println("      XX                  XX      ");
+		System.out.println("     X                      X     ");
+		System.out.println("    X                        X    ");
+		System.out.println("   X                          X   ");
+		System.out.println("  X                            X  ");
+		System.out.println("  X                            X  ");
+		System.out.println("  X     XXXXXX      XXXXXX     X  ");
+		System.out.println("  X   XXXXXXXXX    XXXXXXXXX   X  ");
+		System.out.println("  X  XXXXXXXXXX    XXXXXXXXXX  X  ");
+		System.out.println("  X  XXXXXXXXXX    XXXXXXXXXX  X  ");
+		System.out.println("  X  XXXXXXXXXX    XXXXXXXXXX  X  ");
+		System.out.println("   X  XXXXXXXX  XX  XXXXXXX   X   ");
+		System.out.println("   X    XXXXXX XXXX XXXXX     X   ");
+		System.out.println("    X       X  XXXX  X       X    ");
+		System.out.println("    X     XX   XXXX   XX     X    ");
+		System.out.println("   X    X      XXXX      X    X   ");
+		System.out.println("   X    XX              XX    X   ");
+		System.out.println("   X  XXX XX          XX XXX  X   ");
 		System.out.println("")
+		
 
 	}
 
