5b73f267d3a76d563fb4d51cbfce61a823388702
JackReis01 <jackreis2019@gmail.com>
2018-09-07 08:51:26 -0500

    updated text

---
+++ Chapter 2/src/debugging/FixDebugTwo2.java
@@ -0,0 +1,15 @@
+package debugging;
+
+public class FixDebugTwo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a, b;
+		a = 7;
+		b = 4;
+		
+		
+	}
+
+}
