39b3488da74ff863153cbd51bd63f324c835ace1
JackReis01 <jackreis2019@gmail.com>
2018-09-07 09:08:10 -0500

    updated text

---
+++ Chapter 2/src/gameZone/Sans.java
@@ -0,0 +1,18 @@
+package gameZone;
+
+public class Sans {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("           XXXXXXXXXXXX          ");
+		System.out.println("        XXX            XXX       ");
+		System.out.println("      XX                  XX     ");
+		System.out.println("     X                      X    ");
+		System.out.println("    X                        X   ");
+		System.out.println("   X                          X  ");
+		System.out.println("")
+
+	}
+
+}
