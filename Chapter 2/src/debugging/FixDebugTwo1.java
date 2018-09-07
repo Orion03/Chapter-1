fe698440897d5e7c7c8aecccd139645fe664b715
JackReis01 <jackreis2019@gmail.com>
2018-09-07 08:46:40 -0500

    updated text

--- Chapter 2/src/debugging/FixDebugTwo1.java
+++ Chapter 2/src/debugging/FixDebugTwo1.java
@@ -4,7 +4,12 @@ public class FixDebugTwo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int oneInt = 315;
+		double oneDouble = 12.4;
+		char oneChar = 'A';
+		
+		
 	}
 
 }
