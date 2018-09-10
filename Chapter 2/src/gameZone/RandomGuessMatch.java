a915ae0cc921399e7435174d3add9b477c07f1cd
JackReis01 <jackreis2019@gmail.com>
2018-09-10 09:14:20 -0500

    updated text

---
+++ Chapter 2/src/gameZone/RandomGuessMatch.java
@@ -0,0 +1,28 @@
+package gameZone;
+
+import java.util.Scanner;
+
+public class RandomGuessMatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int randomInt;
+		int MAX = 5;
+		int Guess;
+		
+		Scanner input = new Scanner(System.in);
+		
+		randomInt = 1 +(int)(Math.random() * MAX);
+		
+		System.out.println("Pick a number between 1 and 5 >> ");
+		Guess = input.nextInt();
+		
+		if (Guess==randomInt) {
+			System.out.println("You got it right! The number is " + Guess + ".");
+		}
+		
+		if (Guess!=randomInt) {
+	}
+
+}
